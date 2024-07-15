0d3d489e8f8b755d3c37e88252f24c40036b7ec5
paulobressan <paulo.bressan@outlook.com>
2024-07-15 15:14:38 -0300

    feat: added kafka event key

--- src/drivers/event/mod.rs
+++ src/drivers/event/mod.rs
@@ -1,9 +1,9 @@
-use anyhow::Result;
+use anyhow::{bail, Error, Result};
 use rdkafka::{
     consumer::{CommitMode, Consumer, StreamConsumer},
     ClientConfig, Message,
 };
-use std::{path::Path, sync::Arc};
+use std::{borrow::Borrow, path::Path, sync::Arc};
 use tracing::{error, info};
 
 use crate::{
@@ -34,9 +34,8 @@ pub async fn subscribe(config: EventConfig) -> Result<()> {
     loop {
         match consumer.recv().await {
             Err(err) => error!(error = err.to_string(), "kafka subscribe error"),
-            Ok(message) => {
-                if let Some(payload) = message.payload() {
-                    let event: Event = serde_json::from_slice(payload)?;
+            Ok(message) => match message.borrow().try_into() {
+                Ok(event) => {
                     match event {
                         Event::ProjectCreated(namespace) => {
                             projects::create::create_cache(project_cache.clone(), namespace).await?
@@ -50,8 +49,31 @@ pub async fn subscribe(config: EventConfig) -> Result<()> {
                     };
                     consumer.commit_message(&message, CommitMode::Async)?;
                 }
-            }
+                Err(err) => {
+                    error!(error = err.to_string(), "fail to convert message to event");
+                    consumer.commit_message(&message, CommitMode::Async)?;
+                }
+            },
         };
+    }
+}
+
+impl TryFrom<&rdkafka::message::BorrowedMessage<'_>> for Event {
+    type Error = Error;
+
+    fn try_from(
+        value: &rdkafka::message::BorrowedMessage<'_>,
+    ) -> std::result::Result<Self, Self::Error> {
+        let Some(key) = value.key() else {
+            bail!("event with empty key")
+        };
+        let key = String::from_utf8(key.to_vec())?;
+
+        let Some(payload) = value.payload() else {
+            bail!("event with empty payload")
+        };
+        let event = Event::from_key(&key, payload)?;
+        Ok(event)
     }
 }
 
