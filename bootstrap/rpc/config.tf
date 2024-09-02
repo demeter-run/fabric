bb23443d393f5a31b4c9e88dfdd891f68df76f7d
Felipe Gonzalez <gonzalezz_felipe@hotmail.com>
2024-09-02 16:50:24 -0300

    fix: Update bootstrapping for auth and stripe additions (#106)

--- bootstrap/rpc/config.tf
+++ bootstrap/rpc/config.tf
@@ -10,13 +10,17 @@ resource "kubernetes_config_map_v1" "fabric_rpc_config" {
       {
         port = local.port,
         // If we change the consumer, we must rebuild the cache.
-        db_path        = "/var/cache/${var.consumer_name}.db",
-        broker_urls    = var.broker_urls
-        consumer_name  = var.consumer_name
-        kafka_username = var.kafka_username
-        kafka_password = var.kafka_password
-        topic          = var.kafka_topic
-        secret         = var.secret
+        db_path             = "/var/cache/${var.consumer_name}.db",
+        broker_urls         = var.broker_urls
+        consumer_name       = var.consumer_name
+        kafka_username      = var.kafka_username
+        kafka_password      = var.kafka_password
+        topic               = var.kafka_topic
+        secret              = var.secret
+        auth0_client_id     = var.auth0_client_id
+        auth0_client_secret = var.auth0_client_secret
+        auth0_audience      = var.auth0_audience
+        stripe_api_key      = var.stripe_api_key
       }
     )}"
   }
