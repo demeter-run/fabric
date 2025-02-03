0601b5910836a2ec82298f561caa5bf30ce2cf2e
gonzalezzfelipe <gonzalezz_felipe@hotmail.com>
2025-02-03 10:55:57 -0300

    chore: Dont notify on E2E test

--- src/domain/notify/mod.rs
+++ src/domain/notify/mod.rs
@@ -1,7 +1,16 @@
-use super::{auth::Auth0Driven, event::Event, Result};
+use super::{
+    auth::Auth0Driven, event::Event, project::cache::ProjectDrivenCache,
+    resource::cache::ResourceDrivenCache, Result,
+};
 use std::sync::Arc;
 
 #[async_trait::async_trait]
 pub trait NotifyDriven: Send + Sync {
-    async fn notify(&self, evt: Event, auth_driven: Arc<dyn Auth0Driven>) -> Result<()>;
+    async fn notify(
+        &self,
+        evt: Event,
+        auth_driven: Arc<dyn Auth0Driven>,
+        resource_cache: Arc<dyn ResourceDrivenCache>,
+        project_cache: Arc<dyn ProjectDrivenCache>,
+    ) -> Result<()>;
 }
