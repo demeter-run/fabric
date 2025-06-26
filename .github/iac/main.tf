114af29840c5025470af475b9d8d610efaa440d7
paulobressan <paulo.bressan@outlook.com>
2025-06-26 11:41:45 -0300

    chore: updated iac github workflow

--- .github/iac/main.tf
+++ .github/iac/main.tf
@@ -35,6 +35,7 @@ variable "stripe_api_key" {}
 variable "slack_webhook_url" {}
 variable "email_ses_access_key_id" {}
 variable "email_ses_secret_access_key" {}
+variable "balius_pg_url" {}
 
 
 locals {
@@ -58,6 +59,7 @@ locals {
   email_ses_access_key_id     = var.email_ses_access_key_id
   email_ses_secret_access_key = var.email_ses_secret_access_key
   email_ses_verified_email    = "no-reply@demeter.run"
+  balius_pg_url               = var.balius_pg_url
 }
 
 module "fabric_rpc" {
@@ -83,6 +85,7 @@ module "fabric_rpc" {
   email_ses_secret_access_key = local.email_ses_secret_access_key
   email_ses_verified_email    = local.email_ses_verified_email
   url_prefix                  = "rpc"
+  balius_pg_url               = local.balius_pg_url
 }
 
 module "fabric_daemon" {
